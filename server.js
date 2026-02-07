adf80a0e811705227281d9d7100a4bd1842e3746
kunal_sharma_003 <kunalsharma00541@gmail.com>
2026-02-07 12:20:17 +0530

    First commit add all dependices tthat needed in this assignment

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send("hello world ");
+});
+
+
+
+const port = 5000;
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`);
+})
