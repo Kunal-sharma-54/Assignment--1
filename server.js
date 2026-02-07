586cbc5aac51b99c7c63cdedecf4f1bbee222ec1
kunal_sharma_003 <kunalsharma00541@gmail.com>
2026-02-07 13:29:01 +0530

    successfully make getproducts route

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
+require("dotenv").config()
 const app = express();
+const products = require("./routes/productRoutes")
+
+
+
+//middelware
+app.use(express.json());
 
 
 app.get('/',(req,res)=>{
     res.send("hello world ");
 });
 
+app.use('/api',products);
 
 
-const port = 5000;
+
+
+
+const port = process.env.PORT;
 
 app.listen(port,()=>{
-    console.log(`server is running on ${port}`);
+    console.log(`server is running on ${port}`)
 })
