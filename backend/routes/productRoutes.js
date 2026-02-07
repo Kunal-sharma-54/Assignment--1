b607343f1e9ad6d5fec8f3818682c3d687501742
kunal_sharma_003 <kunalsharma00541@gmail.com>
2026-02-07 21:08:05 +0530

    Delete backend directory

--- backend/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const {getproducts} = require("../controllers/demodataController")
-
-const router = express.Router();
-
-router.get('/products',getproducts);
-
-
-module.exports = router;
